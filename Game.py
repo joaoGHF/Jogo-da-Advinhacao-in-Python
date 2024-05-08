3cf322eeb5cf2bff6f57d0f699e5ee6bb4f82e47
JoaoGHF <joao.gabrielhorsth.fonseca@escola.pr.gov.br>
2024-05-08 14:58:18 -0300

    Fix bugs and add functionalities

--- Game.py
+++ Game.py
@@ -1,3 +1,4 @@
+# Projeto do aluno João Gabriel Horsth Fonseca
 import random;
 import time;
 import os;
@@ -21,6 +22,51 @@ initial_chances_count = int(0);
 
 user_attempt = int(0);
 number_state = str("");
+
+emoticon_1 = str("¯\_(ツ)_/¯");
+emoticon_2 = str("( ‾ʖ̫‾)");
+emoticon_3 = str("(￣＿￣)");
+emoticon_4 = str("(°_°) ");
+emoticon_5 = str("ツ");
+emoticon_6 = str("(✪‿✪)");
+emoticon_7 = str("⊙ ０⊙");
+
+# Função que define os valores dos emoticons 
+def setEmoticons():
+    global emoticon_1;
+    global emoticon_2;
+    global emoticon_3;
+    global emoticon_4;
+    global emoticon_5;
+    global emoticon_6;
+    global emoticon_7;
+    
+    emoticon_1 = str("¯\_(ツ)_/¯");
+    emoticon_2 = str("( ‾ʖ̫‾)");
+    emoticon_3 = str("(￣＿￣)");
+    emoticon_4 = str("(°_°) ");
+    emoticon_5 = str("ツ");
+    emoticon_6 = str("(✪‿✪)");
+    emoticon_7 = str("⊙ ０⊙");
+
+# Função que limpa os valores dos emoticons
+def clearEmoticons():
+    global emoticon_1;
+    global emoticon_2;
+    global emoticon_3;
+    global emoticon_4;
+    global emoticon_5;
+    global emoticon_6;
+    global emoticon_7;
+    
+    emoticon_1 = str("");
+    emoticon_2 = str("");
+    emoticon_3 = str("");
+    emoticon_4 = str("");
+    emoticon_5 = str("");
+    emoticon_6 = str("");
+    emoticon_7 = str("");
+    
 
 # Função que configura o jogo
 def configureGame():
@@ -48,8 +94,7 @@ def configureGame():
         max_range = int(10);
         chances_count = int(4);
         tips_count = int(3);
-        print("\nIsto será fácil para você.");
-        return;
+        print(f"\nIsto será fácil para você {emoticon_3}");
             
     elif(difficulty_value == 2):
         difficulty_name = str("Médio");
@@ -57,8 +102,7 @@ def configureGame():
         max_range = int(100);
         chances_count = int(7);
         tips_count = int(3);
-        print("\nVamos ter alguma diversão.");
-        return;
+        print(f"\nVamos ter alguma diversão {emoticon_5}");
             
     elif(difficulty_value == 3):
         difficulty_name = str("Díficil");
@@ -66,8 +110,7 @@ def configureGame():
         max_range = int(1000);
         chances_count = int(10);
         tips_count = int(5);
-        print("\nVocê realmente acredita que consegue?");
-        return;
+        print(f"\nVocê realmente acredita que consegue? {emoticon_1}");
             
     elif(difficulty_value == 4):
         difficulty_name = str("Extremo");
@@ -75,8 +118,7 @@ def configureGame():
         max_range = int(1000);
         chances_count = int(10);
         tips_count = int(7);
-        print("\nÉ melhor você desistir, ou acha que vou facilitar para você?");
-        return;
+        print(f"\nÉ melhor você desistir, ou acha que vou facilitar para você? {emoticon_2}");
             
     elif(difficulty_value == 5):
         # Customizado
@@ -91,11 +133,10 @@ def configureGame():
             
         chances_count = int(input("Digite a quantidade de chances: "));
         tips_count = int(input("Digite a quantidade de dicas: "));
-        print("\nBem... Veremos se você irá criar algo inovador.");
-        return;
+        print(f"\nBem... Veremos se você criou algo inovador {emoticon_7}");
     else:
         os.system("cls");
-        print("Valor inválido para a dificuldade.\n");
+        print(f"Valor inválido para a dificuldade.\n");
         configureGame();
         
     initial_tips_count = tips_count;
@@ -108,29 +149,29 @@ def runGame():
     global initial_chances_count;
     global initial_tips_count;
     global sorted_number;
-        
+    
     chances_count = initial_chances_count;
     tips_count = initial_tips_count;
     sorted_number = random.randint(min_range, max_range);
     
     is_win = False;
     
-    print("Estou pensando em um número... Vamos começar!");
+    print(f"Estou pensando em um número... Vamos começar! {emoticon_6}");
     time.sleep(0.2);
     print(f"Lembrando que o intervalo escolhido é de {min_range} até {max_range}");
     
-    while chances_count >= 0:
+    while chances_count > 0:
         user_attempt = int(input("\nQual número você acha que estou pensando? "));
         chances_count -= 1;
     
         if (user_attempt != sorted_number):
-            print(f"Resposta errada, você ainda tem {chances_count} chances.");
+            print(f"Resposta errada {emoticon_4}, você ainda tem {chances_count} chances.");
             
             if (tips_count > 0):
-                wants_tip = bool(input(f"você possui {tips_count} dicas restando, deseja usar uma? [ENTER para passar] "));
+                wants_tip = bool(input(f"você possui {tips_count} dicas restando {emoticon_5}, deseja usar uma? [ENTER para passar] "));
                 
                 if (wants_tip):
-                    if (user_attempt > sorted_number):
+                    if (user_attempt < sorted_number):
                         number_state = "maior";
                     else:
                         number_state = "menor";
@@ -138,12 +179,12 @@ def runGame():
                     print(f"Aqui esta sua dica. O número é {number_state} que {user_attempt}.");
                     tips_count -= 1;
                 else:
-                    print("Como você não quer usar sua dica, vamos prosseguir");
+                    print(f"Como você não quer usar sua dica, vamos prosseguir {emoticon_1}");
             else:
-                print("Você está cada vez mais próximo do fim, e está sem dicas.");
+                print(f"Você está cada vez mais próximo do fim, e está sem dicas {emoticon_4}");
                 
         else:
-            print(f"Você acertou, eu realmente estava pensando no número {sorted_number}.");
+            print(f"Você acertou {emoticon_6}, eu realmente estava pensando no número {sorted_number}.");
             is_win = True;
             input("Aperte ENTER para continuar");
             break;
@@ -173,6 +214,7 @@ while True:
         print(f"{9*'='}MENU=DO=JOGO{9*'='}");
         game_menu_value = int(input("1 - Iniciar Jogo.\n" +
                                     "2 - Reconfigurar Jogo.\n" +
+                                    "3 - Habilitar/Desabilitar emoticons.\n" +
                                     "-1 - Desistir.\n" +
                                     "Escolha: "));
         
@@ -180,9 +222,17 @@ while True:
             runGame();
         elif (game_menu_value == 2):
             configureGame();
+        elif (game_menu_value == 3):
+            if (emoticon_1 == ""):
+                setEmoticons();
+                print(f"Emoticons ativados {emoticon_5}\n");
+            else:
+                clearEmoticons();
+                print(f"Emoticons desativados\n");
+        
         elif (game_menu_value == -1):
-            print("Você desistiu? Nunca crio expectativas, mas espero que você tente novamente.");
+            print(f"Você desistiu? Nunca crio expectativas, mas espero que você tente novamente {emoticon_7}");
             exit();
         else:
             os.system("cls");
-            print("Valor inválido para a o menu do jogo.\n");
+            print(f"Valor inválido para a o menu do jogo.\n");
